934aa0774ec29727d280a0fc3570ebcc4549f8f9
arivukkodi <arivukkodi@lendsmart.ai>
2019-05-30 18:25:27 +0530

    receipe app

--- react/src/App.js
+++ react/src/App.js
@@ -1,93 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {Navbar,NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes';
+import DishDetail  from './components/DishdetailComponent'
 
-class Myapp extends React.Component {
-  constructor(props) {
+
+class App extends Component {
+  constructor(props){
     super(props);
     this.state = {
-      name:'',
-      class:'',
-      tamilmark:'',
-      englishmark:'',
-      mathsmark:'',
-      sciencemark:'',
-      socialmark:'',
-    }
+      dishes: DISHES,
+      comments: DISHES
+
+    };
   }
-
-
-
-  onChangeName = (e: React.ChangeEvent < HTMLInputElement > ) => {
-    this.setState({
-      name: e.target.value
-    });
-  };
-
-  onChangeClass = (e: React.ChangeEvent < HTMLInputElement > ) => {
-    this.setState({
-      class: e.target.value
-    });
-  };
-
-  onChangeTamilmark = (e: React.ChangeEvent < HTMLInputElement > ) => {
-    this.setState({
-      tamilmark: e.target.value
-    });
-  };
-
-  onChangeEnglishmark = (e: React.ChangeEvent < HTMLInputElement > ) => {
-    this.setState({
-      englishmark: e.target.value
-    });
-  };
-
-  onChangeMathsmark = (e: React.ChangeEvent < HTMLInputElement > ) => {
-    this.setState({
-      mathsmark: e.target.value
-    });
-  };
-
-  onChangeSciencemark = (e: React.ChangeEvent < HTMLInputElement > ) => {
-    this.setState({
-      sciencemark: e.target.value
-    });
-  };
-
-  onChangeSocialmark = (e: React.ChangeEvent < HTMLInputElement > ) => {
-    this.setState({
-      socialmark: e.target.value
-    });
-  };
-
-
-  handleSubmit = () => {
-    const {
-      name
-    } = this.state;
-    push("/find");
-  };
-
   render() {
-
     return (
-
-      <fieldset>
-               <legend><b>Student Input Section:</b></legend>
-               Name:<input type="text" value = {this.state.name} onChange = {this.onChangeName}/>
-               Class:<input type="text" value = {this.state.class} onChange = {this.onChangeClass}/> <br/><br/>
-               <table border="1">
-                   <tr><td>Tamil</td><td><input type="text" value = {this.state.tamilmark} onChange = {this.onChangeTamilmark}/></td></tr>
-                   <tr><td>English</td><td><input type="text" value = {this.state.englishmark} onChange = {this.onChangeEnglishmark}/></td></tr>
-                   <tr><td>Maths</td><td><input type="text" value = {this.state.mathsmark} onChange = {this.onChangeMathsmark}/></td></tr>
-                   <tr><td>Science</td><td><input type="text" value = {this.state.sciencemark} onChange = {this.onChangeSciencemark}/></td></tr>
-                   <tr><td>Socialscience</td><td><input type="text" value = {this.state.socialmark} onChange = {this.onChangeSocialmark}/></td></tr>
-               </table><br/><br/>
-               <input type="button" onclick= {this.handleSubmit } value="Get Result"/>
-               </fieldset>
-
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes ={this.state.dishes} />
+        <DishDetail dish ={this.state.dish} comments={this.state.comments} />
+      </div>
     );
   }
 }
-
-
-
-export default Myapp;
+ export default App;
