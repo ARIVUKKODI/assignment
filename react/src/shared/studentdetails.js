29d57421b1ef9774bb592705f32ed737f5bbeac9
ARIVUKKODI <49359124+ARIVUKKODI@users.noreply.github.com>
2019-07-09 15:34:50 +0530

    student details

---
+++ react/src/shared/studentdetails.js
@@ -0,0 +1,79 @@
+const students = [
+
+      {
+      id:1,
+      name:'Anu',
+      marks:
+      [
+      {
+          id:1,
+          tamil: "90",
+          english: "80",
+          maths: "89",
+          science: "80",
+          social:"78"
+          },
+
+      ] },
+
+      {
+      id:2,
+      name:'anbu',
+      marks:
+      [
+      {
+          id:2,
+          tamil: "95",
+          english: "70",
+          maths: "79",
+          science: "87",
+          social:"88"
+          },
+
+      ] },
+      {
+      id:3,
+      name:'banu',
+      marks:
+      [
+      {
+          id:3,
+          tamil: "80",
+          english: "90",
+          maths: "75",
+          science: "97",
+          social:"81"
+          },
+
+      ] },
+      {
+      id:4,
+      name:'chitra',
+      marks:
+      [
+      {
+          id:4,
+          tamil: "89",
+          english: "76",
+          maths: "67",
+          science: "90",
+          social:"70"
+          },
+
+      ] },
+      {
+      id:5,
+      name:'chitra',
+      marks:
+      [
+      {
+          id:5,
+          tamil: "89",
+          english: "76",
+          maths: "67",
+          science: "90",
+          social:"70"
+          },
+
+      ]  },
+]
